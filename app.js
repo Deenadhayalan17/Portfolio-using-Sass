229b45d55a9b26bd14e2ebea0ae4113b64fbbab0
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-04-21 12:52:11 +0530

    added header

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ toggle.addEventListener('click', () => {
 	console.log("clicked")
 	ul.classList.toggle('nav__active');
 });
+//name effect
+const nameElement = document.querySelector('.header__data__contents__heading');
+const nameText = "Deenadhayalan D"
+let start = 0;
+
+const typeWrite = () => {
+    if(start < nameText.length) {
+		nameElement.innerHTML += nameText.charAt(start);
+		start++;
+		setTimeout(typeWrite, 300);
+	}
+}
+typeWrite();
